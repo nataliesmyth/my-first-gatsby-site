b2b4ae27146a8901432249f7995fab55f38523e2
Nataliesmyth <nataliesmyth11@gmail.com>
2025-03-27 18:25:36 -0400

    added static image to homepage via image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage 
+        alt="A white shih tzu posing between two roses looking happy"
+        src="https://wallpaperaccess.com/full/2969912.jpg"
+      />
     </Layout>
   )
 }
