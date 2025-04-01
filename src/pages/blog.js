1eee871d1b0f61af1f21c88a5b43825bb3768d54
Nataliesmyth <nataliesmyth11@gmail.com>
2025-04-01 16:54:54 -0400

    rendered blog posts on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,38 @@
 import * as React from 'react'
-// Follow the steps below to add a list of post filenames to your blog page.
-// step 1: import graphql tag from the Gatsby package.
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-// step 3: add in the data prop to the function defenition.
-// then replace the placeholder <p> element with a list of the filenames for your posts.
-// Use js array .map() method to iterate over the nodes array and render the filename for each post. 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
-// step 2: define and export your page query. Copy the query you built in GraphiQL.
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
       }
     }
-  }`
+  }
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
