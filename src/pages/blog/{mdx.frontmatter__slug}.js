f3c54b3d46d05be8891c656bdb9d03c72e34db49
Nataliesmyth <nataliesmyth11@gmail.com>
2025-04-04 16:11:11 -0400

    updated blog page to link to each post

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,11 +1,9 @@
-// Create blog post page template
-// 1. create this file src/pages/{mdx.frontmatter__slug}.js
-// 2. create a basic page component
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = () => {
+  // console.log('hello', BlogPost)
   return (
     <Layout pageTitle="Super Cool Blog Posts">
       <p>My blog post contents will go here (eventually).</p>
